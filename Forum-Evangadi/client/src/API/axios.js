17aa210058bb2b26c3cd88723d27511cacf44696
11339844 <sol339844@gmail.com>
2024-08-16 14:26:55 -0400

    client updated

--- Forum-Evangadi/client/src/API/axios.js
+++ Forum-Evangadi/client/src/API/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create an instance of axios
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:7500", // Update with your base URL
+  baseURL: "https://forum-evangadi-1.onrender.com", // Update with your base URL
 });
 
 // Request interceptor to include token in headers
