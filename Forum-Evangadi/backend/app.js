206d596263a6ad78997160241cb88b4d67694bfd
11339844 <sol339844@gmail.com>
2024-08-16 14:43:00 -0400

    final

--- Forum-Evangadi/backend/app.js
+++ Forum-Evangadi/backend/app.js
@@ -12,7 +12,7 @@ const answerRoutes = require("./routes/answerRoute");
 const questionRoutes = require("./routes/questionRoute");
 
 // JSON middleware to extract JSON data
-app.use(cors(cors));
+app.use(cors());
 app.use(express.json());
 // const corsOptions = {
 //   origin: "http://localhost:5173", // Replace with the allowed origin
